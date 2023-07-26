ff6fcc5be944deed036c76e58364cbf04a7f3862
Muhammad Nofil <nofil.khan037@gmail.com>
2023-07-26 17:17:23 +0500

    server

--- server.mjs
+++ server.mjs
@@ -1,8 +1,10 @@
 console.log("this is express server");
 
 import express from 'express';
-const app = express()
+import cors from 'cors';
 
+const app = express();
+app.use(cors())
 
 // http://192.168.1.106:3000
 
@@ -13,6 +15,39 @@ app.get('/', (req, res) => {
 app.get('/profile', (req, res) => {
   console.log('this is profile !', new Date());
   res.send('this is  profile !' + new Date())
+})
+
+app.get('/weather/:cityName', (req, res) => {
+  console.log('this is profile !', new Date());
+  let weatherData = {
+    karachi :{
+      city : 'karachi',
+      tempInC : 30,
+      humidity : 56 ,
+      high : 35,
+      low : 25
+    },
+    london : {
+      city : 'london',
+      tempInC : 38,
+      humidity : 54 ,
+      high : 42,
+      low : 21
+    }
+  }
+  
+  let userInputCityName = req.params.cityName.toLowerCase(); 
+  console.log('userInputCityName:',userInputCityName);
+
+  let weatherDataToSend = weatherData[userInputCityName]
+  
+  if( weatherDataToSend){
+    res.send( weatherDataToSend);
+  }else{
+    res.status(404)
+    .send(`weather data is not available`);
+  }
+
 })
 
 
